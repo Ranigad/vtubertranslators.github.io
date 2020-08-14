e7e83b5f3868cfd2e7701a3f3949a2d66414ed6e
Ranigad <li.w.charles@gmail.com>
2020-08-14 16:08:13 -0700

    Added comments on sort section, added check for duplicate IDs and names and print duplicates to console

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,20 @@
 function createList(channels) {
+    
+    // Sorting channel list by 'name' attribute, taking note if duplicate exists during sort. Keeps array of all duplicate values and prints to console (feel free to change)
+    let duplicate = [];
     channels.sort((channel1, channel2) => {
-        if (channel1['name'].toLowerCase() > channel2['name'].toLowerCase()) {
+        let c1 = channel1['name'].toLowerCase(),
+            c2 = channel2['name'].toLowerCase();
+        if ( channel1['id'] == channel2['id'] || c1 == c2 ) {
+            duplicate.push(c1);
+            return 0;
+        } else if ( c1 > c2 ) {
             return 1;
-	} else {
+        } else {
             return -1;
-	}
+        }
     });
+    console.log(duplicate);
 
     return channels.map((channel) => `
         <a href="https://www.youtube.com/channel/${channel.id}/videos" class="channel ta-c" target="_blank">
