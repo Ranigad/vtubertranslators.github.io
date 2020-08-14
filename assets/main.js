293bc3f921d77cb858d703b8ab5544078b8d2584
Ranigad <li.w.charles@gmail.com>
2020-08-14 15:45:58 -0700

    Adding sort feature to ensure list of channels is sorted alphabetically assuming no changes to the structure of the channels.js file

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,12 @@
 function createList(channels) {
+    channels.sort((channel1, channel2) => {
+        if (channel1['name'].toLowerCase() > channel2['name'].toLowerCase()) {
+            return 1;
+	} else {
+            return -1;
+	}
+    });
+
     return channels.map((channel) => `
         <a href="https://www.youtube.com/channel/${channel.id}/videos" class="channel ta-c" target="_blank">
             ${channel.name}
